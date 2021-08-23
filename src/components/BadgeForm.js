fbe1649a938843cc14ccaf8253061d640c5f5bf4
chris <cseiter@gmail.com>
2021-08-23 02:15:20 -0400

    create new badge form

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -31,13 +31,42 @@ function BadgeForm({onAddBadge}) {
         .then(onAddBadge);
     }
 
-
-
-
-
-
-
-
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <h3>Enter New Badge</h3>
+                <input 
+                type="text"
+                name="name"
+                onChange={handleChange}
+                value={formData.name}
+                placeholder="Enter badge name, underscore for spaces"
+                />
+                <br />
+                <input
+                type="text"
+                name="eagle"
+                onChange={handleChange}
+                value={formData.eagle}
+                placeholder="Is this for Eagle? yes/no"
+                />
+                <br />
+                <input
+                type="text"
+                name="earned"
+                onChange={handleChange}
+                value={formData.earned}
+                placeholder="Is this badge already earned? yes/no"
+                />
+                <br />
+                <input 
+                type="submit"
+                name="submit"
+                value="Create New Badge"
+                />
+            </form>
+        </div>
+    );
 };
 
 export default BadgeForm;
