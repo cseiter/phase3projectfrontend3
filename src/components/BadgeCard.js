6ce77fd25e6a926b3f4817e873978a3e93d524d4
chris <cseiter@gmail.com>
2021-08-23 01:31:56 -0400

    add patch fetch to updatebadge function

--- src/components/BadgeCard.js
+++ src/components/BadgeCard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 function BadgeCard({badge, onDeleteBadge, onUpdateBadge}) {
     const {id, badges_name, badges_is_earned, badges_is_eagle} = badge;
@@ -13,10 +13,18 @@ function BadgeCard({badge, onDeleteBadge, onUpdateBadge}) {
     }
 
     function handleEarnClick() {
-        const updateObj = {earned: badge.badges_is_earned = "yes",
-    };
-
-
+        const updateObj = {earned: badge.badges_is_earned = "yes"
+        };
+        fetch('http://localhost:9393/badges/${id}', {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(updateObj),
+        })
+        .then((r) => r.json())
+        .then(onUpdateBadge);
+    }
 
 
 
