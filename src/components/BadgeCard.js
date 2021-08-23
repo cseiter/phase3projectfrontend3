5a145d7d2a983c18cc1ace186a6f1718ec17c734
chris <cseiter@gmail.com>
2021-08-23 06:59:56 -0400

    add console.log and fix apostrophe to accent in delete function and accent in earn functions

--- src/components/BadgeCard.js
+++ src/components/BadgeCard.js
@@ -4,10 +4,11 @@ function BadgeCard({badge, onDeleteBadge, onUpdateBadge}) {
     const {id, badges_name, badges_is_earned, badges_is_eagle} = badge;
 
     function handleDeleteClick() {
-        fetch('http://localhost:9393/badges/${id}', {
+        fetch(`http://localhost:9393/badges/${id}`, {
             method: "DELETE",
         })
         .then((r) => r.json())
+        .then(console.log({id}))
         .then(() => {onDeleteBadge(badge);
         });
     }
@@ -15,7 +16,7 @@ function BadgeCard({badge, onDeleteBadge, onUpdateBadge}) {
     function handleEarnClick() {
         const updateObj = {earned: badge.badges_is_earned = "yes"
         };
-        fetch('http://localhost:9393/badges/${id}', {
+        fetch(`http://localhost:9393/badges/${id}`, {
             method: "PATCH",
             headers: {
                 "Content-Type": "application/json",
