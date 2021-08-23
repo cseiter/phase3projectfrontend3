ebfb874376800c2dc883b4f7e4d77a5f616fef77
chris <cseiter@gmail.com>
2021-08-23 06:54:15 -0400

    change submit form values to match db

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -3,9 +3,9 @@ import React, {useState} from "react";
 function BadgeForm({onAddBadge}) {
 
     const [formData, setFormData] = useState({
-        name: "",
-        earned: "",
-        eagle: "",
+        badges_name: "",
+        badges_is_earned: "",
+        badges_is_eagle: "",
     });
 
     function handleChange(e) {
@@ -38,25 +38,25 @@ function BadgeForm({onAddBadge}) {
                 <h3>Enter New Badge</h3>
                 <input 
                 type="text"
-                name="name"
+                name="badges_name"
                 onChange={handleChange}
-                value={formData.name}
+                value={formData.badges_name}
                 placeholder="Enter badge name, underscore for spaces"
                 />
                 <br />
                 <input
                 type="text"
-                name="eagle"
+                name="badges_is_eagle"
                 onChange={handleChange}
-                value={formData.eagle}
+                value={formData.badges_is_eagle}
                 placeholder="Is this for Eagle? yes/no"
                 />
                 <br />
                 <input
                 type="text"
-                name="earned"
+                name="badges_is_earned"
                 onChange={handleChange}
-                value={formData.earned}
+                value={formData.badges_is_earned}
                 placeholder="Is this badge already earned? yes/no"
                 />
                 <br />
