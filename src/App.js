dee16de0b3ac83c509b3e60a4ff6b11f8727e129
chris <cseiter@gmail.com>
2021-08-23 02:30:37 -0400

    import new components into App.js and beign writing helper functions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import React, {useEffect, useState} from 'react';
 import BadgeContainer from './components/BadgeContainer';
+import Header from "./components/Header";
+import BadgeForm from './components/BadgeForm';
+
 
 function App() {
-
+  const [showForm, setShowForm] = useState(false);
   const [badges, setBadges] = useState([]);
 
   useEffect(() => { 
@@ -11,6 +14,20 @@ function App() {
     .then((r) => r.json())
     .then(console.log(setBadges));
   },[]);
+
+  function handleClick() {
+    setShowForm((showForm) => !showForm);
+  }
+
+  function handleAddBadge(newBadge) {
+    setBadges([...badges, newBadge]);
+  }
+
+  function handleUpdateBadge(updatedBadge) {
+    const updatedBadges = badges.map((badge) =>
+    badge.id === updatedBadge.id ? updatedBadge : badge);
+    setBadges(updatedBadges);
+  }
 
   return (
     <div className="App">
