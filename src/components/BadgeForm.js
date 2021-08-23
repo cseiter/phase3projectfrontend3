91299ec722c1d95e3be6a7882658500dc2bb287b
chris <cseiter@gmail.com>
2021-08-23 02:06:25 -0400

    begin building out form functions

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -2,6 +2,19 @@ import React, {useState} from "react";
 
 function BadgeForm({onAddBadge}) {
 
+    const [formData, setFormData] = useState({
+        name: "",
+        earned: "",
+        eagle: "",
+    });
+
+    function handleChange(e) {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    }
+
 
 
 
