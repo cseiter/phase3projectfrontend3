29033b624111ec2620775366ae5c9f865dee413c
chris <cseiter@gmail.com>
2021-08-23 02:25:25 -0400

    create and fill in Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div>
+            <img
+            src="https://www.clipartmax.com/png/small/120-1209481_what-kinds-of-activities-did-you-participate-in-as-boy-scouts-of.png"
+            alt="Eagle Scout Badge"
+            />
+        </div>
+    );
+}
+
+export default Header;
