d304ecf9ad8097bf71bf7ed3b0b2c5ad46fc4d9d
chris <cseiter@gmail.com>
2021-08-23 01:44:04 -0400

    create initial container layout for badge info

--- src/components/BadgeContainer.js
+++ src/components/BadgeContainer.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import BadgeCard from './BadgeCard';
 
-
-
+function BadgeContainer( {badges, onDeleteBadge, onUpdateBadge}) {
+    const badgeCards = badges.map((badge) => (
+        <BadgeCard
+            key={badge.id}
+            badge={badge}
+            onDeleteBage={onDeleteBadge}
+            onUpdateBadge={onUpdateBadge}
+        />
+    ));
+    return <div>{badgeCards}</div>;
+}
 export default BadgeContainer;
